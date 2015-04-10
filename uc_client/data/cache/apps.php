2856db468827991dbbcacd935a6c7198d8ca87c1
edwardfhsiao <edwardfhsiao@gmail.com>
2015-04-10 11:05:37 -0400

    initialize

---
+++ uc_client/data/cache/apps.php
@@ -0,0 +1,26 @@
+<?php
+$_CACHE['apps'] = array (
+  1 => 
+  array (
+    'appid' => '1',
+    'type' => 'ECSHOP',
+    'name' => 'ECSHOP',
+    'url' => 'http://localhost/ecshop/',
+    'ip' => '',
+    'charset' => 'utf-8',
+    'synlogin' => '1',
+    'extra' => '',
+  ),
+  2 => 
+  array (
+    'appid' => '2',
+    'type' => 'PBCC Store',
+    'name' => '北美商城',
+    'url' => 'http://localhost/beimeishangcheng/',
+    'ip' => '',
+    'charset' => 'utf-8',
+    'synlogin' => '1',
+    'extra' => '',
+  ),
+  'UC_API' => 'http://localhost/ucenter',
+);
